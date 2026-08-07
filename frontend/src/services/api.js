d6c1ad61ba6138bcc715f511dc9a67b811e3aaad
SangeethaL-M <sangeethaclp@gmail.com>
2026-08-07 11:34:48 +0530

    event

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'https://event-nexus-backend-co38.onrender.com';
+const API = axios.create({
+  baseURL: 'http://127.0.0.1:5000/api',
+});
 
 // Interceptor to attach token automatically with "Bearer " prefix
 API.interceptors.request.use((config) => {
